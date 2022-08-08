57c72bd0716715d12a01c6a5d3fba815e7ea75ad
YarLikViD <yarlistratenko@yandex.ru>
2022-08-08 15:04:31 +0300

    feat: proxy static

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 load_dotenv()
 
@@ -144,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
